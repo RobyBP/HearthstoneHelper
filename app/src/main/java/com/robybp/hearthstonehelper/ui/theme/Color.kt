fe28bc777e4e8a291bb9ec77a97e23a3a989f119
Robert Baricevic-Petrus <robert.baricevicpetrus@gmail.com>
2022-03-08 00:52:47 +0100

    Initial commit

---
+++ app/src/main/java/com/robybp/hearthstonehelper/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.robybp.hearthstonehelper.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightBrown = Color(0xff8c5238)
+val DarkBrown = Color(0xff7e4931)
+val LightRed = Color(0xffc33121)
+val DarkRed = Color(0xff882014)
