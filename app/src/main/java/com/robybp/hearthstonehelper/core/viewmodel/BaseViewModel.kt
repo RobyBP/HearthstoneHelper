aaf2d971799e0ef6e3807e3846eb898c61000aff
Robert Baricevic-Petrus <robert.baricevicpetrus@gmail.com>
2022-03-08 15:03:58 +0100

    Implement core components

---
+++ app/src/main/java/com/robybp/hearthstonehelper/core/viewmodel/BaseViewModel.kt
@@ -0,0 +1,16 @@
+package com.robybp.hearthstonehelper.core.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.e(this.javaClass.name, "An error occurred: $throwable")
+    }
+
+    private val bgScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    private fun runInBackground(block: suspend () -> Unit) = bgScope.launch { block() }
+}
