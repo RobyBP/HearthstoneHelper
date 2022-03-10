c219ec8e59c5c5528a72957be3f375e5890c87e0
Robert Baricevic-Petrus <robert.baricevicpetrus@gmail.com>
2022-03-10 11:54:44 +0100

    Remove preview function

--- app/src/main/java/com/robybp/hearthstonehelper/MainActivity.kt
+++ app/src/main/java/com/robybp/hearthstonehelper/MainActivity.kt
@@ -3,9 +3,6 @@ package com.robybp.hearthstonehelper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.robybp.hearthstonehelper.ui.main.MainScreen
 import com.robybp.hearthstonehelper.ui.theme.HearthstoneHelperTheme
 
@@ -17,18 +14,5 @@ class MainActivity : ComponentActivity() {
                 MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HearthstoneHelperTheme {
-        Greeting("Android")
     }
 }
